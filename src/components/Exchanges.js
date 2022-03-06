79a1bfdfac44067c57c414720f4ad18555c945c4
jarangit <jaran.dch@gmail.com>
2022-03-06 22:32:45 +0700

    resty

--- src/components/Exchanges.js
+++ src/components/Exchanges.js
@@ -4,7 +4,7 @@ import React from 'react'
 
 import {useGetExchangesQuery} from '../services/cryptoApi'
 const Exchanges = () => {
-  const {data, isFetching} = useGetExchangesQuery()
+  const {data} = useGetExchangesQuery()
   console.log(data);
   return (
     <div>Exchanges</div>
